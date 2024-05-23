c1d0ef31ccf09b62a696a54824341c1f917bb96a
Fraile2 <145047146+Fraile2@users.noreply.github.com>
2024-05-23 08:27:24 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-class Padre():
-    def __init__(self, nombre) -> None:
-        self.nombre=nombre
-
-class Hijo(Padre):
-    def __init__(self, nombre, apellidos) -> None:
-        super().__init__(nombre)
-        self.apellidos=apellidos
